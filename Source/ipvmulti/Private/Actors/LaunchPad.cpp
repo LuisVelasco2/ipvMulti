e38dea092bdbdf4b975120cfb71463f161787f95
LuisVelasco2 <133169809+LuisVelasco2@users.noreply.github.com>
2025-04-02 11:04:18 -0600

    Overlap jump pad

--- Source/ipvmulti/Private/Actors/LaunchPad.cpp
+++ Source/ipvmulti/Private/Actors/LaunchPad.cpp
@@ -14,6 +14,8 @@ ALaunchPad::ALaunchPad()
 	OverlapComp= CreateDefaultSubobject<UBoxComponent>("BoXCompon");
 	RootComponent = OverlapComp;
 	MeshComp ->SetupAttachment(OverlapComp);
+	LaunchForce = 1000;
+	LaunchAngle = 90;
 }
 
 // Called when the game starts or when spawned
@@ -25,9 +27,13 @@ void ALaunchPad::BeginPlay()
 
 void ALaunchPad::OverlapLaunchpad(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
 {
+	FRotator LaunchDirection = GetActorRotation();
+	LaunchDirection.Pitch += LaunchAngle;
+	FVector LaunchVelocity = LaunchDirection.Vector()*LaunchForce;
 	ACharacter* MyCharacter = Cast<ACharacter>(OtherActor);
 	if (MyCharacter)
 	{
+			MyCharacter->LaunchCharacter(LaunchVelocity, true,true);
 		if (GEngine)
 		{
 			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, TEXT("Overlap"));
